e3df5dbe8ab92c67b829797abf9222d27c720054
Yantar-f <shevchenko9542@gmail.com>
2023-04-12 14:07:11 +0600

    Added simple authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'homepage#index'
+  get 'persons/profile', as: 'user_root'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
